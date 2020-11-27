0626ced183cf74c1f7d105dfae1eea4f200b1884
ptlong1 <38217232+ptlong1@users.noreply.github.com>
2020-11-27 18:29:10 +0700

    a
    a

--- app/src/main/java/com/hcmus/callapp/ui/MainActivity.java
+++ app/src/main/java/com/hcmus/callapp/ui/MainActivity.java
@@ -2,7 +2,10 @@ package com.hcmus.callapp.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.hcmus.callapp.R;
 
@@ -12,5 +15,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = (Button) findViewById(R.id.findButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openCallingActivity();
+            }
+        });
     }
+    private void openCallingActivity() {
+        Intent intent = new Intent(this, CallingActivity.class);
+        startActivity(intent);
+    }
+
 }
